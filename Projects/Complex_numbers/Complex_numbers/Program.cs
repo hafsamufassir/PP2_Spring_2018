f0522f16b516a34ac2c962175bc2535136ffd02a
hafsamufassir <hafsamufassir@gmail.com>
2018-01-29 01:03:06 +0600

    Class Complex

--- Projects/Complex_numbers/Complex_numbers/Program.cs
+++ Projects/Complex_numbers/Complex_numbers/Program.cs
@@ -4,20 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ComplexClass
+namespace Complex_numbers
 {
+    class Complex 
+    {
+        int a;
+        int b;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Complex c1 = new Complex(2, 3);
-            Complex c2 = new Complex(7, 9);
+            string line1 = Console.ReadLine();
+            string[] arr1 = line1.Split(' ');
 
-            Complex result = c1 + c2;
+            int a = int.Parse(arr1[0]);
+            int b = int.Parse(arr1[1]);
 
-            Console.WriteLine(result);
+            string line2 = Console.ReadLine();
+            string[] arr2 = line2.Split(' ');
 
+            int c = int.Parse(arr2[0]);
+            int d = int.Parse(arr2[1]);
 
+            Complex c1 = new Complex(a, b);
+            Complex c2 = new Complex(c, d);
+
+            Complex summ = c1 + c2;
+            Complex diff = c1 - c2;
+            Complex prod = c1 * c2;
+            Complex div = c1 / c2;
+
+            Console.WriteLine("Summ of complex numbers is equal to " + summ);
+            Console.WriteLine("Difference of complex numbers is equal to " + diff);
+            Console.WriteLine("Product of complex numbers is equal to " + prod);
+            Console.WriteLine("Quotient of complex numbers is equal to " + d);
 
             Console.ReadKey();
         }
