d0bded42795b97f40ee836b523e313f7de7fc331
hafsamufassir <hafsamufassir@gmail.com>
2018-03-04 22:01:17 +0600

    Trying timer

---
+++ Projects/Timer/Timer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Timer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer t = new Timer();
+            t.Interval = 1000;
+            t.Elapsed += DoIt;
+            t.Start();
+
+            //Timer t2 = new Timer();
+            //t2.Interval = 2000;
+            //t2.Elapsed += DoIt2;
+            //t2.Start();
+
+            while (true)
+            {
+
+            }
+
+            t.Stop();
+        }
+
+        private static void DoIt(object sender, ElapsedEventArgs e)
+        {
+            Console.Clear();
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+            Console.ReadKey();
+
+        }
+
+        private static void DoIt2(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("ok2");
+        }
+    }
+}
