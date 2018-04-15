35e1bc80f8c2a43720c0562236d93e8e85531bc0
Hafsa Mufassir <hafsamufassir@gmail.com>
2018-04-15 21:03:49 +0600

    Lab 10, graphics

---
+++ source/repos/Calc_example/Calc_example/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcWindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        Brain brain = new Brain();
+        public Form1()
+        {
+            InitializeComponent();
+            brain.invoker = ShowInfo;
+        }
+        public void ShowInfo(string msg)
+        {
+            textBox1.Text = msg;
+        }
+        public void BtnClick(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            brain.Process(btn.Text);
+        }
+    }
+}
