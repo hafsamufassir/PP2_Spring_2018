0966ec14579c1ef1893143f9a6cc634eecc1d4ee
Hafsa Mufassir <hafsamufassir@gmail.com>
2018-05-05 13:13:11 +0600

    all primes in label before number that is given in textbox

---
+++ source/repos/PrimesLabel/PrimesLabel/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PrimesLabel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(231, 92);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(294, 223);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(351, 388);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(70, 25);
+            this.label.TabIndex = 1;
+            this.label.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 778);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ source/repos/PrimesLabel/PrimesLabel/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimesLabel
+{
+    public partial class Form1 : Form
+    {
+        int num;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            //textBox.Text = "";
+            label.Text = "";
+            num = int.Parse(textBox.Text);
+            //num = int.Parse(s); 
+            List<int> list = new List<int>();
+            string s;
+            if(textBox.Text == num.ToString())
+            {
+                for(int i = 1; i < num; i++)
+                { 
+                    if (IsPrime(i))
+                    {
+                        list.Add(i);
+                    }
+                }
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    label.Text += list[i].ToString() + " " ;
+                }
+            }
+        }
+
+        static bool IsPrime(int a)
+        {
+            if(a == 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(a); i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
